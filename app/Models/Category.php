c2589cc116be6e34c6da1393e9bf8f2fddd7872a
jkryszto00 <jakubkryszto@gmail.com>
2024-11-29 17:18:43 +0100

    category model, seeder and factory

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+    use SoftDeletes;
+    use HasSlug;
+
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
