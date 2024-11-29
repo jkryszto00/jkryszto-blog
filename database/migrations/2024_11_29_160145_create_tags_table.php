1365a77a7fc67b45662d73541c579d11a303bd54
jkryszto00 <jakubkryszto@gmail.com>
2024-11-29 17:18:48 +0100

    tag model, seeder and factory

--- database/migrations/2024_11_29_160145_create_tags_table.php
+++ database/migrations/2024_11_29_160145_create_tags_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
